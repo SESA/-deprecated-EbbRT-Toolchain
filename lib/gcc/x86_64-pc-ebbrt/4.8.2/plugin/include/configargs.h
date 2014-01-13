5a439e10abe2a5e8bea421497982384c2ff0028d
Dan Schatzberg <schatzberg.dan@gmail.com>
2014-01-13 15:35:45 -0500

    initial commit

---
+++ lib/gcc/x86_64-pc-ebbrt/4.8.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.8.2/configure --target=x86_64-pc-ebbrt --prefix=/home/dschatz/Work/SESA/EbbRT/ext/toolchain/ --enable-languages=c,c++ --disable-libssp --without-headers --disable-nls --with-newlib --enable-threads=ebbrt";
+static const char thread_model[] = "ebbrt";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
